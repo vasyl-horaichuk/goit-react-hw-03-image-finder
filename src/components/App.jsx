b72bac1595ad514be8cae9a8987a1e2c9822e364
Vasyl Horaichuk <vg.createam@gmail.com>
2023-01-28 15:56:18 +0200

    search form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { Searchbar } from './Searchbar/Searchbar';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    test: [],
   };
 
   render() {
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,19 +1,35 @@
-export const Searchbar = () => {
-  return (
-    <header className="searcbar">
-      <form className="form">
-        <button type="submit" className="button">
-          <span className="button-label">Search</span>
-        </button>
+import { Component } from 'react';
 
-        <input
-          className="input"
-          type="text"
-          autocomplete="off"
-          autofocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
-};
+export class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handlerChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+  render() {
+    const { query } = this.state;
+
+    return (
+      <header className="searcbar">
+        <form className="form">
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            name="query"
+            value={query}
+            onChange={this.handlerChange}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
