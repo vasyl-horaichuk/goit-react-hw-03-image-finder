6b3326257ce9d469cb61168ab122f73085b3c97b
Vasyl Horaichuk <vg.createam@gmail.com>
2023-01-29 23:26:27 +0200

    casa

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,10 @@
 import { ButtonLoadMore } from './Button.styled';
+import { TiArrowSync } from 'react-icons/ti';
 
 export const Button = ({ onClick }) => {
   return (
     <ButtonLoadMore type="button" onClick={onClick}>
+      <TiArrowSync size={25} stroke="white" />
       Load more
     </ButtonLoadMore>
   );
--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -8,7 +8,10 @@ export const ButtonLoadMore = styled.button`
     background-color: #ff0027;
     transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
     text-align: center;
-    display: inline-block;
+    display: flex;
+    gap:5px;
+    align-items:center;
+    justify-content: center;
     color: #fff;
     border: 0;
     text-decoration: none;
