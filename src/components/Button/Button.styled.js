39aa832d34218d864a3fcc8e9720c3dce24093fa
Vasyl Horaichuk <vg.createam@gmail.com>
2023-01-29 23:00:23 +0200

    scdd

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -2,8 +2,10 @@ import styled from 'styled-components'
 
 export const ButtonLoadMore = styled.button`
     padding: 8px 16px;
+    margin-left:auto;
+    margin-right:auto;
     border-radius: 4px;
-    background-color: #e31837;
+    background-color: #ff0027;
     transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
     text-align: center;
     display: inline-block;
@@ -16,13 +18,13 @@ export const ButtonLoadMore = styled.button`
     line-height: 24px;
     font-style: normal;
     font-weight: 500;
-    min-width: 180px;
+    min-width: 280px;
     box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
 
 
     :hover,
     :focus {
-        opacity: 0.6;
+        background-color: #e31837;
     }
 `
